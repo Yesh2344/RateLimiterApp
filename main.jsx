331f386c9686fa439c0e82a9d2027d31405d1195
Yesh2344 <125935833+Yesh2344@users.noreply.github.com>
2026-04-23 17:18:49 +0000

    feat: add input validation helpers

--- main.jsx
+++ main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { RateLimiter } from './utils/rateLimiter';
 
+// Updated - v6.9.0
 ReactDOM.render(
   <React.StrictMode>
     <App />
